aefa0805e5b0b3b6552c82b2af0190ebd94a62cb
agipacz-gh <czapiga.joanna@gmail.com>
2019-12-01 15:24:41 +0100

    grupowanie folderow

---
+++ niedziela/programik2.py
@@ -0,0 +1,5 @@
+liczby =list()
+for l in range(2000,3001):
+    if ((l%7== 0) and (l%5!= 0)):
+        liczby.append(l)
+print(liczby)
---
+++ sobota/zjazd3/cw2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+z= 'kawa '
+y= 'z mlekiem'
+
+print(z+y)
+
+x= '''kawa
+z mlekiem'''
+
+print(x)
